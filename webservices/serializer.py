3880b7e7f0f282dc338f47cd2a6ad00b2fd24f12
Kelly Tobar <kellytobar7@gmail.com>
2018-08-29 12:27:45 -0500

    2-tienda con microservicios-29-agosto

--- webservices/serializer.py
+++ webservices/serializer.py
@@ -5,7 +5,7 @@ class producto_serializer(serializers.HyperlinkedModelSerializer): #con hyperlin
 #class producto_serializer(serializers.ModelSerializer): #hay que hacer una consulta adicional
 	class Meta:
 		model=Producto
-		fields=('url','id','nombre','precio','foto','status','marca')
+		fields=('url','id','nombre','precio','foto','status','stock','marca')
 
 
 class marca_serializer(serializers.HyperlinkedModelSerializer): #con hyperlinked se basa en el modelo y lo va a serializar
